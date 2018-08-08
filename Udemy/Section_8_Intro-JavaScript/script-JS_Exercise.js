175a57811756f7fb995fdf88c7a9ba1a2556a5f2
ManassehBBTNA <29733650+ManassehBBTNA@users.noreply.github.com>
2018-08-08 16:06:37 +0100

    Continuing with JavaScript

---
+++ Udemy/Section_8_Intro-JavaScript/script-JS_Exercise.js
@@ -0,0 +1,8 @@
+var firstName = prompt ("What is your name?");
+var lastName = prompt ("What is your last name?");
+var age = prompt ("How old are you?");
+//alert("Nice to meet you " + firstName + " " + lastName);
+//alert("Your " + age + " today, Happy Birthday!");
+var fullName = firstName + " " + lastName;
+console.log("Nice to meet you " + fullName);
+console.log("Your " + age + " today, Happy Birthday!");
